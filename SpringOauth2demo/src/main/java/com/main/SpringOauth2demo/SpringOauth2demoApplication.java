264228e2c538acb122410b50677a64ba6ba03813
sagarboyal <sboyal12@gmail.com>
2024-10-30 20:22:59 +0530

    initial commit

---
+++ SpringOauth2demo/src/main/java/com/main/SpringOauth2demo/SpringOauth2demoApplication.java
@@ -0,0 +1,13 @@
+package com.main.SpringOauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2demoApplication.class, args);
+	}
+
+}
---
+++ SpringOauth2demo/src/test/java/com/main/SpringOauth2demo/SpringOauth2demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.SpringOauth2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
